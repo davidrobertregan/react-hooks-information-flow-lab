8d0dc30c7992697e952811a0d7d239c90c7c5723
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2021-09-01 11:26:04 -0500

    commit tree d04c90e4647af000a4db4775e6dee892a17b7d86

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+function Header({ onDarkModeClick }) {
+    return (
+        <header>
+            <h2>Shopster</h2>
+            <button onClick={onDarkModeClick}>
+                {onDarkModeClick ? "Dark" : "Light"} Mode
+            </button>
+        </header>
+    )
+}
+
+export default Header
